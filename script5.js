618410eeccdc439f4ee9d8ad27c4ea7b9d82f204
Amirhossein <117942712+AmirDevboy@users.noreply.github.com>
2023-11-16 00:34:38 +0330

    JS-Practice-5
    دو عدد از ورودی گرفته و کوچکتر و بزرگتر را مشخص کنید؟

---
+++ script5.js
@@ -0,0 +1,15 @@
+
+//سوال
+// دو عدد از ورودی گرفته و کوچکتر و بزرگتر را مشخص کنید؟
+
+let number1 = 11;
+let number2 = 40;
+
+if (number1 > number2) {
+    console.log("Max is :", number1);
+    console.log("Min is :", number2);
+}
+else{
+    console.log("Max is :", number2);
+    console.log("Min is :", number1);
+}
