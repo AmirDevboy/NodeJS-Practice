4186c81e1f762c7b4ca55a986c72e69835953886
Amirhossein <117942712+AmirDevboy@users.noreply.github.com>
2023-11-16 00:34:04 +0330

    JS-Practice-4
    سه عدد از ورودی دریافت کرده و میانگین آن ها را محاسبه و نمایش دهید؟

---
+++ script4.js
@@ -0,0 +1,11 @@
+
+//سوال
+// سه عدد از ورودی دریافت کرده و میانگین آن ها را محاسبه و نمایش دهید؟
+
+let number1 = 25;
+let number2 = 18;
+let number3 = 33;
+
+let Ave = (number1 + number2 + number3) / 3;
+
+console.log("Ave is :", Ave);
