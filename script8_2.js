75f4217b0bb1fc858932a105d265cffd67d36a2f
Amirhossein <117942712+AmirDevboy@users.noreply.github.com>
2023-11-16 00:39:46 +0330

    JS-Practice-8(Second Method)
    برنامه ای بنویسید که عدد روز هفته را گرفته و مشخص کند چندشنبه است؟
    پاسخ با استفاده از روش آرایه

---
+++ script8_2.js
@@ -0,0 +1,10 @@
+
+//سوال
+//برنامه ای بنویسید که عدد روز هفته را گرفته و مشخص کند چندشنبه است؟
+//پاسخ با استفاده از روش آرایه
+
+let number = 4;
+
+let WeekDays = ["Shanbe", "1 Shanbe", "2 Shanbe", "3 Shanbe", "4 Shanbe", "5 Shanbe", "Jome"];
+
+console.log(WeekDays[number - 1]);
