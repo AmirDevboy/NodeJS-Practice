bdb9975ccaa5ed4ea8d51d60c75bccc21c1b599c
Amirhossein <117942712+AmirDevboy@users.noreply.github.com>
2023-11-16 00:31:46 +0330

    JS-Practice-2
    طول ضلع مربع را دریافت کرده و محیط و مساحت آن را محاسبه کنید؟

---
+++ script2.js
@@ -0,0 +1,11 @@
+
+//سوال
+//طول ضلع مربع را دریافت کرده و محیط و مساحت آن را محاسبه کنید؟
+
+let zel = 5;
+
+let Masahat = zel * zel;
+let Mohit = zel * 4;
+
+console.log("Masahat :", Masahat);
+console.log("Mohit :", Mohit);
