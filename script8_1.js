981b84463a44681ef720c6c62fe5ae1597216344
Amirhossein <117942712+AmirDevboy@users.noreply.github.com>
2023-11-16 00:38:59 +0330

    JS-Practice-8(First Method)
    برنامه ای بنویسید که عدد روز هفته را گرفته و مشخص کند چندشنبه است؟
    پاسخ با استفاده از روش سوئیج

---
+++ script8_1.js
@@ -0,0 +1,32 @@
+
+//سوال
+//برنامه ای بنویسید که عدد روز هفته را گرفته و مشخص کند چندشنبه است؟
+//پاسخ با استفاده از روش سوئیج
+
+let number = 3;
+
+switch (number) {
+    case 1:
+        console.log("Shanbe");
+        break;
+    case 2:
+        console.log("1 Shanbe");
+        break;
+    case 3:
+        console.log("2 Shanbe");
+        break;
+    case 4:
+        console.log("3 Shanbe");
+        break;
+    case 5:
+        console.log("4 Shanbe");
+        break;
+    case 6:
+        console.log("5 Shanbe");
+        break;
+    case 7:
+        console.log("Jome");
+        break;
+    default:
+        console.log("Out of Range");
+}
